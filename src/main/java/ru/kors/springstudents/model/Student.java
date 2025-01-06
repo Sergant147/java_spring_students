b55f2f31f9c14f91dcc8540ee3416fb0556b1d50
Sergant147 <portoslunt@gmail.com>
2025-01-06 17:35:13 +0300

    draft

--- src/main/java/ru/kors/springstudents/model/Student.java
+++ src/main/java/ru/kors/springstudents/model/Student.java
@@ -21,6 +21,13 @@ public class Student {
     @Transient
     private int age;
 
+    public Student(String firstName_, String secondName_, LocalDate dateOfBirth_, String email_){
+        this.firstName = firstName_;
+        this.secondName = secondName_;
+        this.dateOfBirth = dateOfBirth_;
+        this.email = email_;
+    }
+
     public int getAge() {
         return Period.between(dateOfBirth, LocalDate.now()).getYears();
     }
